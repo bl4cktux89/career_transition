1e8ae0bd1ae88e924b25f98a3079d12f5450041c
Vinicius Santana da Silva <bl4cktux89@gmail.com>
2022-01-13 04:01:49 -0300

    cansei, vou dormir

---
+++ 01-learn-a-programming-language/go.md
@@ -0,0 +1,7 @@
+package main
+function main
+> aqui você começa
+> aqui você termina
+
+usando funções
+package.identificator
---
+++ 01-learn-a-programming-language/hello-world.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	numerodebytes, erros := fmt.Println("hello world!")
+	fmt.Println(numerodebytes, erros)
+
+}
